0acb7e0a5e052900eec50e609444865b68e0ec41
cloudlucky_cp <SND\cloudlucky_cp>
2010-12-31 04:10:33 +0000

    test

---
+++ Main/Source/NLib.Tests/Extensions/ArrayExtensionTest.cs
@@ -0,0 +1,19 @@
+﻿namespace NLib.Tests.Extensions
+{
+    using NLib.Extensions;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ArrayExtensionTest
+    {
+        [Test]
+        public void SwapValues1()
+        {
+            var l = new[] { 1, 2, 3, 4, 5, 6 };
+            l.SwapValues(2, 3);
+
+            CollectionAssert.AreEquivalent(l, new[] { 1, 2, 4, 3, 5, 6 });
+        }
+    }
+}
