c676e279ed4672b8a7f994d65b750f9f6c83d4fc
cloudlucky_cp <cloudlucky@users.noreply.github.com>
2011-01-20 02:37:03 +0000

    tests

---
+++ Source/NLib.Tests/Extensions/ComparisonExtensionTest.cs
@@ -0,0 +1,29 @@
+﻿namespace NLib.Tests.Extensions
+{
+    using System;
+
+    using NLib.Extensions;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ComparisonExtensionTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Comparison<int> ec = (i1, i2) => i1 + i2;
+
+            Assert.AreEqual(5, ec(2, 3));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Comparison<int> ec = (i1, i2) => i1 + i2;
+            var t = ec.ToComparer();
+
+            Assert.AreEqual(3, t.Compare(1, 2));
+        }
+    }
+}
