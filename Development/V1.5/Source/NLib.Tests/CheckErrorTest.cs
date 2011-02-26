d6c6db472def870f70232619414e0bf682b2e1dd
cloudlucky_cp <SND\cloudlucky_cp>
2011-02-26 16:26:19 +0000

    CheckError ArgumentException

--- Development/V1.5/Source/NLib.Tests/CheckErrorTest.cs
+++ Development/V1.5/Source/NLib.Tests/CheckErrorTest.cs
@@ -8,9 +8,58 @@
     public class CheckErrorTest
     {
         [Test]
+        public void ArgumentException1()
+        {
+            var foo = 3;
+            CheckError.ArgumentException(foo > 0, "foo");
+            Assert.Pass();
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ArgumentException2()
+        {
+            var foo = -1;
+            CheckError.ArgumentException(foo > 0, "foo");
+            Assert.Fail();
+        }
+
+        [Test]
+        public void ArgumentException3()
+        {
+            var foo = 3;
+            CheckError.ArgumentException(foo > 0, "foo", "foo is negative");
+            Assert.Pass();
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ArgumentException4()
+        {
+            var foo = -1;
+            CheckError.ArgumentException(foo > 0, "foo", "foo is negative");
+            Assert.Fail();
+        }
+
+        [Test]
+        public void ArgumentException5()
+        {
+            var foo = -1;
+            try
+            {
+                CheckError.ArgumentException(foo > 0, "foo", "foo is negative");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual("foo is negative\r\nParameter name: foo", ex.Message);
+            }
+        }
+
+        [Test]
         public void ArgumentNullException1()
         {
             CheckError.ArgumentNullException(string.Empty, "foo");
+            Assert.Pass();
         }
 
         [Test]
@@ -18,12 +67,14 @@
         public void ArgumentNullException2()
         {
             CheckError.ArgumentNullException(null, "foo");
+            Assert.Fail();
         }
 
         [Test]
         public void ArgumentNullException3()
         {
             CheckError.ArgumentNullException(string.Empty, "foo", "foo is null");
+            Assert.Pass();
         }
 
         [Test]
@@ -31,6 +82,7 @@
         public void ArgumentNullException4()
         {
             CheckError.ArgumentNullException(null, "foo", "foo is null");
+            Assert.Fail();
         }
 
         [Test]
