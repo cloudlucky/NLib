e26fec29bbad91c64a806bdd1366b5c987cb8ceb
cloudlucky_cp <cloudlucky@users.noreply.github.com>
2011-02-26 16:26:19 +0000

    CheckError ArgumentException

--- Source/NLib/CheckError.cs
+++ Source/NLib/CheckError.cs
@@ -17,6 +17,27 @@ namespace NLib
     public static class CheckError
     {
         /// <summary>
+        /// Throws <see cref="System.ArgumentException"/> if the <paramref name="condition"/> is false.
+        /// </summary>
+        /// <param name="condition">The condition to check.</param>
+        /// <param name="paramName">Name of the param.</param>
+        public static void ArgumentException(bool condition, string paramName)
+        {
+            ArgumentException(condition, paramName, string.Empty);
+        }
+
+        /// <summary>
+        /// Throws <see cref="System.ArgumentException"/> if the <paramref name="condition"/> is false.
+        /// </summary>
+        /// <param name="condition">The condition to check.</param>
+        /// <param name="paramName">Name of the param.</param>
+        /// <param name="message">The message.</param>
+        public static void ArgumentException(bool condition, string paramName, string message)
+        {
+            Check.Requires<ArgumentException>(condition, message, new { paramName });
+        }
+
+        /// <summary>
         /// Throws <see cref="System.ArgumentNullException"/> if the <paramref name="param"/> is null.
         /// </summary>
         /// <param name="param">The param to check if it's null.</param>
