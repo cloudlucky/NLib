b86d0245c8c9f607ef42e0c64a851a984e37f36b
cloudlucky_cp <SND\cloudlucky_cp>
2011-06-12 15:44:48 +0000

    Merge V1.5

--- Main/Source/NLib.Tests/Collections/Generic/GeneratorTest.cs
+++ Main/Source/NLib.Tests/Collections/Generic/GeneratorTest.cs
@@ -48,9 +48,53 @@
         public void GenerateTest5()
         {
             var collection = Generator.Generate<double>(5, x => 1.0 / ++x);
-            var l = new List<double>(collection);
+
             CollectionAssert.Contains(collection, 1.0 / 1.0);
             CollectionAssert.Contains(collection, 1.0 / 2.0);
+            CollectionAssert.DoesNotContain(collection, 1.0 / 6.0);
+        }
+
+
+        [Test]
+        public void GenerateTest6()
+        {
+            var collection = Generator.Generate(10, x => ++x, 1);
+
+            CollectionAssert.AreEqual(Enumerable.Range(2, 10), collection);
+        }
+
+        [Test]
+        public void GenerateTest7()
+        {
+            var collection = Generator.Generate(0, x => ++x, 1);
+
+            CollectionAssert.AreEqual(Enumerable.Range(0, 0), collection);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GenerateTest8()
+        {
+            var collection = Generator.Generate(-1, x => ++x, 1);
+
+            Assert.True(collection.Any());
+        }
+
+        [Test]
+        public void GenerateTest9()
+        {
+            var collection = Generator.Generate(10, x => --x, 1);
+            Assert.True(collection.Any());
+            Assert.AreEqual(10, collection.Count());
+        }
+
+        [Test]
+        public void GenerateTest10()
+        {
+            var collection = Generator.Generate(5, x => 1.0 / ++x, 1.0);
+
+            CollectionAssert.Contains(collection, 1.0 / 2.0);
+            CollectionAssert.Contains(collection, 2.0 / 3.0);
             CollectionAssert.DoesNotContain(collection, 1.0 / 6.0);
         }
     }
---
+++ Main/Source/NLib.Web/Hosting/WorkerRequest.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WorkerRequest.cs" company=".">
+//   Copyright (c) Cloudlucky. All rights reserved.
+//   http://www.cloudlucky.com
+//   This code is licensed under the Microsoft Public License (Ms-PL)
+//   See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace NLib.Web.Hosting
+{
+    using System;
+    using System.IO;
+    using System.Web;
+    using System.Web.Hosting;
+
+    /// <summary>
+    /// Provides a simple way to process a request an ASP.NET applications outside an Internet Information Services (IIS) application.
+    /// </summary>
+    public sealed class WorkerRequest : MarshalByRefObject, IWorkerRequest
+    {
+        /// <summary>
+        /// Processes the request.
+        /// </summary>
+        /// <param name="page">The page.</param>
+        /// <param name="queryString">The query string.</param>
+        /// <returns>The response.</returns>
+        /// <example>
+        /// <code>
+        /// ProcessRequest("Text.aspx", "Name=Foo");
+        /// </code>
+        /// </example>
+        public TextWriter ProcessRequest(string page, string queryString)
+        {
+            using (var writer = new StringWriter())
+            {
+                var request = new SimpleWorkerRequest(page, queryString, writer);
+                HttpRuntime.ProcessRequest(request);
+                writer.Flush();
+
+                return writer;
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            HttpRuntime.Close();
+        }
+    }
+}
