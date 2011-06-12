f05f410b2945f3bb29a18c3d81f433a757a989c9
cloudlucky_cp <cloudlucky@users.noreply.github.com>
2011-06-12 15:44:48 +0000

    Merge V1.5

---
+++ Source/NLib.Web/Hosting/IWorkerRequest.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IWorkerRequest.cs" company=".">
+//   Copyright (c) Cloudlucky. All rights reserved.
+//   http://www.cloudlucky.com
+//   This code is licensed under the Microsoft Public License (Ms-PL)
+//   See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace NLib.Web.Hosting
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// Provides a simple interface to process a request an ASP.NET applications.
+    /// </summary>
+    public interface IWorkerRequest : IDisposable
+    {
+        /// <summary>
+        /// Processes the request.
+        /// </summary>
+        /// <param name="page">The page.</param>
+        /// <param name="queryString">The query string.</param>
+        /// <returns>The response.</returns>
+        TextWriter ProcessRequest(string page, string queryString);
+    }
+}
--- Source/NLib.Web/Properties/AssemblyInfo.cs
+++ Source/NLib.Web/Properties/AssemblyInfo.cs
@@ -15,12 +15,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NLib")]
-[assembly: AssemblyDescription(".Net utils library")]
+[assembly: AssemblyTitle("NLib.Web")]
+[assembly: AssemblyDescription(".Net utils library for Web")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NLib")]
-[assembly: AssemblyCopyright("Copyright © Cloudlucky 2010")]
+[assembly: AssemblyProduct("NLib.Web")]
+[assembly: AssemblyCopyright("Copyright © Cloudlucky 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
@@ -31,19 +31,19 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6ef25331-771c-4634-8bdf-4041f198b223")]
+[assembly: Guid("bd02fd4f-6475-4e46-ab7b-7a86f3916b44")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
 //      Minor Version 
 //      Build Number
-//      Revision 
+//      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: CLSCompliant(true)]
