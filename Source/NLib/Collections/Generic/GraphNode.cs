d1a1993f4cd7b295756810cbb2effc335ec140ec
cloudlucky_cp <cloudlucky@users.noreply.github.com>
2011-10-02 17:32:19 +0000

    Graph

--- Source/NLib/Collections/Generic/GraphNode.cs
+++ Source/NLib/Collections/Generic/GraphNode.cs
@@ -10,35 +10,66 @@
 namespace NLib.Collections.Generic
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     /// <summary>
     /// Represents a node in a graph.  A graph node contains some piece of data, along with a set of
     /// neighbors. There can be an optional cost between a graph node and each of its neighbors.
     /// </summary>
     /// <typeparam name="T">The type of data stored in the graph node.</typeparam>
-    public class GraphNode<T, TCost>
+    /// /// <typeparam name="TCost">The type of cost.</typeparam>
+    public class GraphNode<T, TCost> : IGraphNode<T, TCost>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GraphNode{T, TCost}"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public GraphNode(T value)
+        {
+            this.Value = value;
+            this.Edges = new List<GraphEdge<T, TCost>>();
+            this.Marker = false;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GraphNode{T, TCost}"/> class.
+        /// </summary>
         protected GraphNode()
         {
         }
 
-        public GraphNode(T value)
+        /// <summary>
+        /// Gets or sets the edges.
+        /// </summary>
+        public ICollection<GraphEdge<T, TCost>> Edges { get; protected set; }
+
+        /// <summary>
+        /// Gets the edges.
+        /// </summary>
+        IEnumerable<IGraphEdge<T, TCost>> IGraphNode<T, TCost>.Edges
         {
-            this.Value = value;
-            this.Edges = new GraphEdgeSet<T, TCost>();
-            this.marker = false;
+            get { return this.Edges; }
         }
 
-        public GraphEdgeSet<T, TCost> Edges { get; protected set; }
+        /// <summary>
+        /// Gets the neighbors.
+        /// </summary>
+        IEnumerable<INode<T>> INode<T>.Neighbors
+        {
+            get { return this.Edges.Select(x => x.To); }
+        }
 
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
         public T Value { get; protected set; }
 
-        public TCost this[T to]
-        {
-            get { return this.Edges[to]; }
-            set { this.Edges[to] = value; }
-        }
-
-        public bool marker { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="GraphNode{T, TCost}"/> is marker.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if marker; otherwise, <c>false</c>.
+        /// </value>
+        public bool Marker { get; set; }
     }
 }
--- Source/NLib/Collections/Generic/IGraph.cs
+++ Source/NLib/Collections/Generic/IGraph.cs
@@ -11,34 +11,128 @@ namespace NLib.Collections.Generic
 {
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Provides the base interface for the abstraction of graph.
+    /// </summary>
+    /// <typeparam name="T">The type of data stored in the graph's nodes.</typeparam>
+    /// <typeparam name="TCost">The type of cost.</typeparam>
     public interface IGraph<T, TCost> : ICollection<T>
     {
-        GraphNode<T, TCost> this[T item] { get; }
+        /// <summary>
+        /// Gets the nodes of the graph.
+        /// </summary>
+        IEnumerable<GraphNode<T, TCost>> Nodes { get; }
+
+        /// <summary>
+        /// Gets the <see cref="IGraphNode{T, TCost}"/> with the specified item.
+        /// </summary>
+        /// <param name="item">The item to search.</param>
+        /// <returns>The item if found; otherwise null.</returns>
+        IGraphNode<T, TCost> this[T item] { get; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="TCost"/> with the specified from.
+        /// </summary>
+        /// <param name="from">The from item.</param>
+        /// <param name="to">The to item.</param>
+        /// <returns>The the cost of the edge between the from and the to items.</returns>
+        TCost this[T from, T to] { get; }
 
         /// <summary>
         /// Adds a directed edge from a GraphNode with one value (from) to a GraphNode with another value (to).
         /// </summary>
+        /// <param name="from">The value of the GraphNode from which the directed edge eminates.</param>
+        /// <param name="to">The value of the GraphNode to which the edge leads.</param>
         void AddDirectedEdge(T from, T to);
-        
-         /// <summary>
-         /// Adds a directed edge from a GraphNode with one value (from) to a GraphNode with another value (to)
+
+        /// <summary>
+        /// Adds a directed edge from a GraphNode with one value (from) to a GraphNode with another value (to)
         /// with an associated cost.
         /// </summary>
+        /// <param name="from">The value of the GraphNode from which the directed edge eminates.</param>
+        /// <param name="to">The value of the GraphNode to which the edge leads.</param>
+        /// <param name="cost">The cost of the edge from "from" to "to".</param>
         void AddDirectedEdge(T from, T to, TCost cost);
-        
+
         /// <summary>
         /// Adds an undirected edge from a GraphNode with one value (from) to a GraphNode with another value (to).
         /// </summary>
+        /// <param name="from">The value of one of the GraphNodetraversee es that is joined by the edge.</param>
+        /// <param name="to">The value of one of the GraphNodes that is joined by the edge.</param>
         void AddUndirectedEdge(T from, T to);
-        
+
         /// <summary>
         /// Adds an undirected edge from a GraphNode with one value (from) to a GraphNode with another value (to)
         /// with an associated cost.
         /// </summary>
+        /// <param name="from">The from value of one of the GraphNodes that is joined by the edge.</param>
+        /// <param name="to">The to value of one of the GraphNodes that is joined by the edge.</param>
+        /// <param name="cost">The cost of the undirected edge.</param>
         void AddUndirectedEdge(T from, T to, TCost cost);
 
-        GraphNode<T, TCost> GetNodeByValue(T value);
+        /// <summary>
+        /// Adds the elements of the specified collection in the bag.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        void AddRange(IEnumerable<T> collection);
 
-        bool TryGetNode(T item, out GraphNode<T, TCost> node);
+        /// <summary>
+        /// Selecting some node as the root and to go through them level-by-level. 
+        /// </summary>
+        /// <param name="item">
+        /// The some root node for start iteration
+        /// </param>
+        /// <returns>
+        /// Iterates throught a collection of nodes.
+        /// </returns>
+        IEnumerable<IGraphNode<T, TCost>> BreathFirstTraversal(IGraphNode<T, TCost> item);
+
+        /// <summary>
+        /// Selecting some node as the root and to go through them level-by-level. 
+        /// </summary>
+        /// <param name="item">
+        /// The value of some root node for start iteration
+        /// </param>
+        /// <returns>
+        /// Iterates throught a collection of nodes.
+        /// </returns>
+        IEnumerable<IGraphNode<T, TCost>> BreathFirstTraversal(T item);
+
+        /// <summary>
+        /// selecting some node as the root and explores as far as possible along each branch before backtracking.
+        /// </summary>
+        /// <param name="item">
+        /// The some node for start iteration
+        /// </param>
+        /// <returns>
+        /// Iterates throught a collection of nodes.
+        /// </returns>
+        IEnumerable<IGraphNode<T, TCost>> DepthFirstTraversal(IGraphNode<T, TCost> item);
+
+        /// <summary>
+        /// Selecting some node as the root and explores as far as possible along each branch before backtracking.
+        /// </summary>
+        /// <param name="item">
+        /// The value of some root node for start iteration
+        /// </param>
+        /// <returns>
+        /// Iterates throught a collection of nodes.
+        /// </returns>
+        IEnumerable<IGraphNode<T, TCost>> DepthFirstTraversal(T item);
+
+        /// <summary>
+        /// Gets the edge between the two nodes.
+        /// </summary>
+        /// <param name="from">The from node.</param>
+        /// <param name="to">The to node.</param>
+        /// <returns>The edge between the two nodes; otherwise null;</returns>
+        IGraphEdge<T, TCost> GetEdge(T from, T to);
+
+        /// <summary>
+        /// Gets the node.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <returns>The node; otherwise null.</returns>
+        IGraphNode<T, TCost> GetNode(T item);
     }
 }
